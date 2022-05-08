4311ac64a5d89fe99fa4a11cff7140f7a19a694c
Fareez Ahamed <fareez.ahamed@gmail.com>
2022-05-08 17:41:36 +0530

    useRef, forwardRef, useImperativeHandle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useImperativeHandle, useRef, useState } from "react";
+import "./App.css";
+
+const Input = React.forwardRef(({ value, onChange }, ref) => {
+  const inputRef = useRef();
+  useImperativeHandle(ref, () => {
+    return {
+      focusField() {
+        inputRef.current.focus();
+      },
+    };
+  });
+  return <input ref={inputRef} type="text" value={value} onChange={onChange} />;
+});
+
+function List() {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const inputRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setList((ls) => [...ls, input]);
+    setInput("");
+    inputRef.current.focusField();
+  };
+
+  return (
+    <React.Fragment>
+      <ul>
+        {list.map((l) => (
+          <li key={l}>{l}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <Input
+          ref={inputRef}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+    </React.Fragment>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List />
     </div>
   );
 }
